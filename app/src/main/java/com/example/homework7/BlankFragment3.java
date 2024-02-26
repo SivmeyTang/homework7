d863079ce736316a4441524460ec19df19e32d5b
SothanaRith <heangsothanarith@gmail.com>
2024-02-26 19:45:55 +0700

    homework7

---
+++ app/src/main/java/com/example/homework7/BlankFragment3.java
@@ -0,0 +1,80 @@
+package com.example.homework7;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.sql.Time;
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment3#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment3 extends DialogFragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public BlankFragment3() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment3.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BlankFragment3 newInstance(String param1, String param2) {
+        BlankFragment3 fragment = new BlankFragment3();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank3, container, false);
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar cd = Calendar.getInstance();
+        int hour = cd.get(Calendar.HOUR_OF_DAY);
+        int mid = cd.get(Calendar.MINUTE);
+        TimePickerDialog.OnTimeSetListener timeSetListener = (TimePickerDialog.OnTimeSetListener) getActivity();
+        return new TimePickerDialog(getActivity(), timeSetListener, hour, mid, DateFormat.is24HourFormat((getActivity())
+        ));
+    }
+}
